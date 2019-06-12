e5893789a8d9d1ad4b72602c7f2c70296c48128b
peraza97 <omarperaza19@gmail.com>
2019-06-11 18:17:07 -0700

    removed extra files

--- open_test.py
+++
@@ -1,6 +0,0 @@
-filename = "hi.txt"
-import io
-words_list = []
-with io.open(filename, 'r', encoding='iso-8859-15') as f:
-    words_list = [word for line in f for word in line.split()]
-print(words_list)
